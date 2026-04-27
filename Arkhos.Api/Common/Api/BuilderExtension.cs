df014741dbc25d7fde957f0abab105725a457b53
AntoniEduardoSO <aeso@ic.ufal.br>
2026-04-27 17:47:41 -0300

    nao veja

--- Arkhos.Api/Common/Api/BuilderExtension.cs
+++ Arkhos.Api/Common/Api/BuilderExtension.cs
@@ -74,13 +74,12 @@ public static class BuilderExtension
             if (isProduction)
             {
                 Console.WriteLine("[DEBUG] NUVEM DETECTADA: Usando Turso LibSql (Hardcoded)");
-                
-                string tursoUrl = "libsql://arkhos-antonieduardoso.aws-us-east-1.turso.io";
+
+                string tursoUrl = "https://arkhos-antonieduardoso.aws-us-east-1.turso.io";
                 string authToken = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicm8iLCJleHAiOjE3ODUwODU1NDEsImlhdCI6MTc3NzMwOTU0MSwiaWQiOiIwMTlkY2YxNi01OTAxLTc5NzQtOTI0Zi0xZThiNmZmMjhiOTciLCJyaWQiOiI1MGU0NDk4ZS04YWNhLTQ0NDUtYmU1Mi0zMjA5NmI4NDM0MTYifQ.qf6I8DV05ZP_orB7FJ1d2SYGfi22vqDRE3bkWzxiJGdukoLY9Tm76cdU31yhrq7cmgFjfq_OyQCAIauR0aFHCA";
-                
+
                 string connectionString = $"{tursoUrl};jwt={authToken}";
-                
-                // Usando a sintaxe específica que a documentação pede
+
                 options.UseLibSql(connectionString);
             }
             else
